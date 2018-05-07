cf486c9205fc14b79c18d27585d94659374160b2
nishantdwivedi <schrodinger744@gmail.com>
2018-05-07 13:47:27 +0530

    1st commit

---
+++ Monk-at-factory.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    long long sum=0;
+    for(int i=0;i<n;i++)
+    {
+
+        int x;
+        cin>>x;
+        sum+=x;
+
+    }
+
+    if(sum==2*(n-1))
+        cout<<"Yes\n";
+    else
+        cout<<"No\n";
+    return 0;
+}
+
